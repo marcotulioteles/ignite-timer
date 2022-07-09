e0a970e98b048ad6ca3ece23936448e417a7effd
Marco Túlio Teles <marcotuliocivileng@gmail.com>
2022-07-08 23:52:33 -0300

    feat: finish home page styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
+
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-      asdfasd
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariant } from './Button.styles'
-
-interface ButtonProps {
-  variant?: ButtonVariant
-}
-
-export function Button({ variant }: ButtonProps) {
-  return <ButtonContainer variant={variant as any}>Enviar</ButtonContainer>
-}
